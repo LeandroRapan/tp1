7b2986c6f3ff9c319b523a569c612e37b4a4227b
LeandroRapan <leandro.rapan@gmail.com>
2022-11-28 23:41:32 -0300

    Comentarios

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+
+//elección del producto
 let valor=0;
 let producto =  prompt("ingrese producto A,B, o C");
 if (producto=="a" || producto=="A"){alert("elegiste una suculenta"); valor+=160} else if (producto=="b" || producto=="B"){alert("elegiste una maceta"); valor+=500} else if (producto=="C" || producto=="c"){alert("elegiste una pala"); valor+=1200} else{alert("ingrese una opcion valida (letras a,b,c)")};
 document.write(`<h3> el valor de la compra es $${valor}</h3>`)
 ;
+//seleccion de numero de cuotas con suma de interes
 let calculoInteres = (valor)=> {
     let coutas= 0;
     let interes= 1;
